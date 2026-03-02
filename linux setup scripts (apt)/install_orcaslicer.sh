e6ec328e9068c0850c935f612ebbe3bc77b6e1f4
myla_00 <74766120+MylaStrev@users.noreply.github.com>
2026-03-02 18:12:35 +0000

    add orcaslicer install script v.1
    first version of orcaslicer install script
    uses wget to download flatpak file from orcaslicer website and then installs via flatpak.

---
+++ linux setup scripts (apt)/install_orcaslicer.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PACKAGES=(
+"wget"
+"flatpak"
+)
+
+# check for wget and flatpak
+
+echo "## CHECKING FOR SNAP"
+for pkg in "${PACKAGES[@]}"; do
+    if dpkg -s "$pkg" &> /dev/null; then
+        echo "## $pkg is already installed, skipping..."
+    else
+        echo "installing $pkg..."
+        sudo apt install -y "$pkg"
+    fi
+done
+
+# download orca slicer from orca website
+
+wget -P Downloads/ https://github.com/OrcaSlicer/OrcaSlicer/releases/download/v2.3.1/OrcaSlicer-Linux-flatpak_V2.3.1_x86_64.flatpak
+
+# check file in downloads
+
+if [ -f Downloads/ ] then
+    echo "file found, now installing..."
+    sudo flatpak install OrcaSlicer-Linux-flatpak_V2.3.1_x86_64.flatpak
+else
+    echo "ORCASLICER FILE NOT FOUND"
+fi
